4f494a1aa3b873aba07dd4f0ddfb92638f05c1fc
Sven Schirmer <sven.glueckspilz@mastersong.de>
2025-08-19 17:33:03 +0200

    tweaking versions again after raspian reinstallation

--- startmeup.sh
+++ startmeup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-export OPENJFX=/usr/lib/jvm/javafx-sdk-17.0.15/lib
+# make sure to apt install openjdk-17-jdk maven openjfx
+export OPENJFX=/usr/share/openjfx/lib/
 java --module-path $OPENJFX --add-modules javafx.controls,javafx.fxml -jar target/IFZ826_LW_Buzzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
