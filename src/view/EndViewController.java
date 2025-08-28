90fb452f4e2310ff47662bcfd2f05603815c074c
Sven Schirmer <sven.glueckspilz@mastersong.de>
2025-08-28 09:43:47 +0200

    quit game after endView

--- src/view/EndViewController.java
+++ src/view/EndViewController.java
@@ -8,15 +8,16 @@ import java.util.stream.Collectors;
 
 import application.GameController;
 import application.Spieler;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class EndViewController{
-	
-	
+
+
 	GameController gameController;
-	
+
 	@FXML
 	Label lblS1Name;
 	@FXML
@@ -29,20 +30,21 @@ public class EndViewController{
 	Label lblS2PunkteGesamt;
 	@FXML
 	Label lblS3PunkteGesamt;
-	
+
 	@FXML AnchorPane rectPlatz1;
 	@FXML AnchorPane rectPlatz2;
 	@FXML AnchorPane rectPlatz3;
-		
+
 	public void setMainController(GameController mainController) {
 		this.gameController = mainController;
 	}
-	
+
 	public void endGameRound(){
-		gameController.showLobbyView();
+		//gameController.showLobbyView();
+		Platform.exit();
 	}
-		
-	public void setSpielerInformation(Set<Spieler> spielerSet) {	
+
+	public void setSpielerInformation(Set<Spieler> spielerSet) {
 		List<Spieler> spielerliste = spielerSet.stream().collect(Collectors.toList());
 		Collections.sort(spielerliste, new Comparator<Spieler>() {
 
@@ -51,40 +53,40 @@ public class EndViewController{
 				return arg1.getPunktestand().get() - arg0.getPunktestand().get();
 			}
 		});
-		
-		Spieler currentSpieler = spielerliste.get(0); 
+
+		Spieler currentSpieler = spielerliste.get(0);
 		String platz1Style = currentSpieler.getName().toString().toLowerCase().replace(" ","");
 		//lblS1Name.getStyleClass().add(platz1Style);
 		rectPlatz1.getStyleClass().add(platz1Style);
 		lblS1Name.setText(currentSpieler.getName().toString());
-		lblS1PunkteGesamt.setText(currentSpieler.getPunktestand().getValue().toString() + " Punkte");		
-		
+		lblS1PunkteGesamt.setText(currentSpieler.getPunktestand().getValue().toString() + " Punkte");
+
 		currentSpieler = spielerliste.get(1);
 		String platz2Style = currentSpieler.getName().toString().toLowerCase().replace(" ","");
 		//lblS2Name.getStyleClass().add(platz2Style);
 		rectPlatz2.getStyleClass().add(platz2Style);
 		lblS2Name.setText(currentSpieler.getName().toString());
 		lblS2PunkteGesamt.setText(currentSpieler.getPunktestand().getValue().toString() + " Punkte");
-		
+
 		if(spielerliste.size() == 2) {
 			lblS3Name.setText("");
 			lblS3PunkteGesamt.setText("");
 			lblS3Name.setStyle("-fx-border-color: none;");
 		}
-		
+
 		else if(spielerliste.size() == 3) {
-			
+
 			System.out.println("Spieler 3 sollte angezeigt werden");
 			currentSpieler = spielerliste.get(2);
 			String platz3Style = currentSpieler.getName().toString().toLowerCase().replace(" ","");
 			//lblS3Name.getStyleClass().add(platz3Style);
 			rectPlatz3.getStyleClass().add(platz3Style);
-		
+
 			lblS3Name.setText(currentSpieler.getName().toString());
 			lblS3PunkteGesamt.setText(currentSpieler.getPunktestand().getValue().toString() + " Punkte");
-			
+
 		}
-	
+
 	}
 
 
