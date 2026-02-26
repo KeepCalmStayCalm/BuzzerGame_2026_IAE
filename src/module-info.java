47d395826841619f70bfea176b608fa9e9fd5ef6
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-02-26 14:28:23 +0100

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,5 @@
 module IAE_Buzzergame {
-    // Java Standard Module
     requires java.prefs;
-    requires java.desktop;
-
-    // Externe Bibliotheken
     requires javafx.controls;
     requires javafx.fxml;
     requires com.pi4j;
@@ -11,9 +7,7 @@ module IAE_Buzzergame {
     requires com.pi4j.plugin.pigpio;
     requires org.slf4j;
 
-    // Erlaubt JavaFX den Zugriff auf deine Controller
     opens application to javafx.graphics, javafx.fxml;
     opens view to javafx.fxml;
-    
     exports application;
 }
