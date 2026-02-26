3811d76c463391aa5d5e6cb8dddd8c775de5f22a
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-02-26 15:37:01 +0100

    Refactor run.sh to update JavaFX module paths

--- assets/run.sh
+++ assets/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 declare SCRIPT_DIR="$(cd "${0%/*}" ; pwd)"
-
-sudo java \
+java \
   -Dglass.platform=gtk \
-  -Djdk.gtk.version=2 \
-  --module-path "${SCRIPT_DIR}:${SCRIPT_DIR}/lib" \
-  --add-modules javafx.controls,javafx.fxml,java.prefs \
-  -jar "${SCRIPT_DIR}/IAE_BuzzerGame-0.0.1-SNAPSHOT.jar"
+  -Djava.library.path="${SCRIPT_DIR}/openjfx/lib" \
+  -Dmonocle.platform.traceConfig=false \
+  -Dprism.verbose=false \
+  -Djavafx.verbose=false \
+  --module-path ".:${SCRIPT_DIR}/openjfx/lib:${SCRIPT_DIR}/lib" \
+  --add-modules javafx.controls \
+  --module buzzer.app/application.GameController $@
