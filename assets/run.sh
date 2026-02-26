0b4807c79617b38d203741cef132f75ea5a065eb
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-02-26 14:13:47 +0100

    Update run.sh

--- assets/run.sh
+++ assets/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 declare SCRIPT_DIR="$(cd "${0%/*}" ; pwd)"
 
-# Start mit sudo für Pi4J Zugriff auf GPIO
+# Wir nutzen sudo für GPIO-Zugriff
 sudo java \
   -Dglass.platform=gtk \
   -Djdk.gtk.version=2 \
   --module-path "${SCRIPT_DIR}:${SCRIPT_DIR}/lib" \
-  --add-modules javafx.controls,javafx.fxml \
+  --add-modules javafx.controls,javafx.fxml,java.prefs \
   -jar "${SCRIPT_DIR}/IAE_Buzzergame_SNAPSHOT_0.0.2.jar"
