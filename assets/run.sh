536d7510f881677f7964b9e67dc2ef8b6386db77
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-02-26 14:03:12 +0100

    Update run.sh

--- assets/run.sh
+++ assets/run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+# Verzeichnis des Skripts ermitteln
 declare SCRIPT_DIR="$(cd "${0%/*}" ; pwd)"
-java \
+
+# Wir starten die JAR-Datei direkt. 
+# Der Classpath beinhaltet die JAR selbst und alle Bibliotheken im lib-Ordner.
+sudo java \
   -Dglass.platform=gtk \
-  -Djava.library.path="${SCRIPT_DIR}/openjfx/lib" \
-  -Dmonocle.platform.traceConfig=false \
-  -Dprism.verbose=false \
-  -Djavafx.verbose=false \
-  --module-path ".:${SCRIPT_DIR}/openjfx/lib:${SCRIPT_DIR}/lib" \
-  --add-modules javafx.controls \
-  --module buzzer.app/application.GameController $@
+  -Djdk.gtk.version=2 \
+  --module-path "${SCRIPT_DIR}/lib:${SCRIPT_DIR}/IAE_Buzzergame_SNAPSHOT_0.0.2.jar" \
+  --add-modules javafx.controls,javafx.fxml \
+  -jar "${SCRIPT_DIR}/IAE_Buzzergame_SNAPSHOT_0.0.2.jar"
