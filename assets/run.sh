2e549637ee4ccbaa0cdb04a8345d2645d839914f
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-02-26 14:09:00 +0100

    Update run.sh

--- assets/run.sh
+++ assets/run.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-# Verzeichnis des Skripts ermitteln
 declare SCRIPT_DIR="$(cd "${0%/*}" ; pwd)"
 
-# Wir starten die JAR-Datei direkt. 
-# Der Classpath beinhaltet die JAR selbst und alle Bibliotheken im lib-Ordner.
+# Start mit sudo für Pi4J Zugriff auf GPIO
 sudo java \
   -Dglass.platform=gtk \
   -Djdk.gtk.version=2 \
-  --module-path "${SCRIPT_DIR}/lib:${SCRIPT_DIR}/IAE_Buzzergame_SNAPSHOT_0.0.2.jar" \
+  --module-path "${SCRIPT_DIR}:${SCRIPT_DIR}/lib" \
   --add-modules javafx.controls,javafx.fxml \
   -jar "${SCRIPT_DIR}/IAE_Buzzergame_SNAPSHOT_0.0.2.jar"
