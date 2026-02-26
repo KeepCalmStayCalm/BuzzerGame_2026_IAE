8ae64d272feca7ea97f888ad9a4acc917b726d26
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-02-26 14:41:46 +0100

    Update JAR file reference in startmeup.sh

--- startmeup.sh
+++ startmeup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
 # make sure to apt install openjdk-17-jdk maven openjfx
 export OPENJFX=/usr/share/openjfx/lib/
-java --module-path $OPENJFX --add-modules javafx.controls,javafx.fxml -jar target/Wiss_BuzzerGame-0.0.1-SNAPSHOT.jar application.RaspiBuzzer
-
+java --module-path $OPENJFX --add-modules javafx.controls,javafx.fxml -jar target/IAE_BuzzerGame-0.0.1-SNAPSHOT.jar application.RaspiBuzzer
