bfbd638f5cf62a3110ab42fd051e064522f5e3ef
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-02-27 08:31:58 +0100

    Refactor StartupViewController for clarity and functionality

--- src/view/StartupViewController.java
+++ src/view/StartupViewController.java
@@ -2,47 +2,43 @@ package view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import application.GameController;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
-public class StartupViewController implements Initializable{
+public class StartupViewController implements Initializable {
 
-	//application getter setter
-	GameController gameController;
-	
-	@FXML Button btnPlay;
+    GameController gameController;
+    
+    @FXML Button btnPlay;
+    @FXML ImageView wissHome;
+    @FXML BorderPane imageRoot;
+    
+    @FXML
+    public void btnLobbyPressed(ActionEvent event) {
+        gameController.showLobbyView();
+    }
+    
+    @FXML public void btnSettingsPressed() {
+        gameController.editSettings();
+    }
+    
+    public void setMainController(GameController gameController) {
+        this.gameController = gameController;
+        btnPlay.requestFocus();
+    }
 
-	@FXML ImageView wissHome;
-	@FXML BorderPane imageRoot;
-	
-	@FXML
-	public void btnLobbyPressed(ActionEvent event) {
-		gameController.showLobbyView();
-		//gameController.spielrundeStarten();
-	}
-	
-	@FXML public void btnSettingsPressed() {
-		gameController.editSettings();
-	}
-	
-	public void setMainController(GameController gameController) {
-		this.gameController = gameController;
-		btnPlay.requestFocus();
-	}
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		if (wissHome.getImage() != null) {
-			wissHome.fitWidthProperty().bind(imageRoot.widthProperty());
-    		//wissHome.fitHeightProperty().bind(imageRoot.heightProperty());
-		}
-	}
-	
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (wissHome != null) {
+            // Bind the background image to fit the container width but maintain ratio
+            wissHome.fitWidthProperty().bind(((AnchorPane)wissHome.getParent()).widthProperty());
+            wissHome.setPreserveRatio(true);
+        }
+    }
 }
