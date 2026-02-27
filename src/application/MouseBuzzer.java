0e97bed9bf165478cdff4dd3f188ff8c4b4bdcd1
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-02-27 11:29:46 +0100

    Update MouseBuzzer.java

--- src/application/MouseBuzzer.java
+++ src/application/MouseBuzzer.java
@@ -3,19 +3,23 @@ package application;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
+/**
+ * Buzzer für Maus/Tastatur-Steuerung im Entwicklungsmodus.
+ * Die Antwort wird manuell per {@link #setAnswer(int)} gesetzt (z.B. durch
+ * einen Button-Click im FXBuzzer-Fenster).
+ */
 public class MouseBuzzer implements IBuzzer {
-    
-    private IntegerProperty answer = new SimpleIntegerProperty(0);
-    
-    public MouseBuzzer() {
-    }
-    
-    public IntegerProperty getAnswer() {
-        return answer;
-    }
 
-    public void setAnswer(int answer) {
-        this.answer.setValue(answer);
-    }
-    
+	private final IntegerProperty answer = new SimpleIntegerProperty(0);
+
+	public MouseBuzzer() { }
+
+	@Override
+	public IntegerProperty getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(int answerIndex) {
+		answer.setValue(answerIndex);
+	}
 }
