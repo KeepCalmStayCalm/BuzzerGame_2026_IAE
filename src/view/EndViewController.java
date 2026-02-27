d933bebc451cbe4594102c11c07e2fbc4c741ab2
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-02-27 14:25:01 +0100

    Update EndViewController.java

--- src/view/EndViewController.java
+++ src/view/EndViewController.java
@@ -8,86 +8,65 @@ import java.util.stream.Collectors;
 
 import application.GameController;
 import application.Spieler;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
-public class EndViewController{
+public class EndViewController {
 
+    @FXML private GameController gameController;
 
-	GameController gameController;
+    @FXML private Label lblS1Name, lblS2Name, lblS3Name;
+    @FXML private Label lblS1PunkteGesamt, lblS2PunkteGesamt, lblS3PunkteGesamt;
+    @FXML private AnchorPane rectPlatz1, rectPlatz2, rectPlatz3;
+    @FXML private Button btnEndGame;
 
-	@FXML
-	Label lblS1Name;
-	@FXML
-	Label lblS2Name;
-	@FXML
-	Label lblS3Name;
-	@FXML
-	Label lblS1PunkteGesamt;
-	@FXML
-	Label lblS2PunkteGesamt;
-	@FXML
-	Label lblS3PunkteGesamt;
+    public void setMainController(GameController mainController) {
+        this.gameController = mainController;
+    }
 
-	@FXML AnchorPane rectPlatz1;
-	@FXML AnchorPane rectPlatz2;
-	@FXML AnchorPane rectPlatz3;
+    public void endGameRound() {
+        gameController.showStartupView();
+    }
 
-	public void setMainController(GameController mainController) {
-		this.gameController = mainController;
-	}
+    public void setSpielerInformation(Set<Spieler> spielerSet) {
+        // Sort players by score (highest first)
+        List<Spieler> spielerliste = spielerSet.stream()
+            .sorted((s1, s2) -> Integer.compare(s2.getPunktestand().get(), s1.getPunktestand().get()))
+            .collect(Collectors.toList());
 
-	public void endGameRound(){
-		gameController.showStartupView();
-		//Platform.exit();
-	}
+        // Placement 1 (Gold)
+        if (spielerliste.size() >= 1) {
+            Spieler s1 = spielerliste.get(0);
+            lblS1Name.setText(s1.getName());
+            lblS1PunkteGesamt.setText(s1.getPunktestand().get() + " Punkte");
+            rectPlatz1.getStyleClass().add(s1.getName().toLowerCase().replace(" ", ""));
+        }
 
-	public void setSpielerInformation(Set<Spieler> spielerSet) {
-		List<Spieler> spielerliste = spielerSet.stream().collect(Collectors.toList());
-		Collections.sort(spielerliste, new Comparator<Spieler>() {
+        // Placement 2 (Silver)
+        if (spielerliste.size() >= 2) {
+            Spieler s2 = spielerliste.get(1);
+            lblS2Name.setText(s2.getName());
+            lblS2PunkteGesamt.setText(s2.getPunktestand().get() + " Punkte");
+            rectPlatz2.getStyleClass().add(s2.getName().toLowerCase().replace(" ", ""));
+            rectPlatz2.setVisible(true);
+        } else {
+            lblS2Name.setText("");
+            lblS2PunkteGesamt.setText("");
+            rectPlatz2.setVisible(false);
+        }
 
-			@Override
-			public int compare(Spieler arg0, Spieler arg1) {
-				return arg1.getPunktestand().get() - arg0.getPunktestand().get();
-			}
-		});
-
-		Spieler currentSpieler = spielerliste.get(0);
-		String platz1Style = currentSpieler.getName().toString().toLowerCase().replace(" ","");
-		//lblS1Name.getStyleClass().add(platz1Style);
-		rectPlatz1.getStyleClass().add(platz1Style);
-		lblS1Name.setText(currentSpieler.getName().toString());
-		lblS1PunkteGesamt.setText(currentSpieler.getPunktestand().getValue().toString() + " Punkte");
-
-		currentSpieler = spielerliste.get(1);
-		String platz2Style = currentSpieler.getName().toString().toLowerCase().replace(" ","");
-		//lblS2Name.getStyleClass().add(platz2Style);
-		rectPlatz2.getStyleClass().add(platz2Style);
-		lblS2Name.setText(currentSpieler.getName().toString());
-		lblS2PunkteGesamt.setText(currentSpieler.getPunktestand().getValue().toString() + " Punkte");
-
-		if(spielerliste.size() == 2) {
-			lblS3Name.setText("");
-			lblS3PunkteGesamt.setText("");
-			lblS3Name.setStyle("-fx-border-color: none;");
-		}
-
-		else if(spielerliste.size() == 3) {
-
-			System.out.println("Spieler 3 sollte angezeigt werden");
-			currentSpieler = spielerliste.get(2);
-			String platz3Style = currentSpieler.getName().toString().toLowerCase().replace(" ","");
-			//lblS3Name.getStyleClass().add(platz3Style);
-			rectPlatz3.getStyleClass().add(platz3Style);
-
-			lblS3Name.setText(currentSpieler.getName().toString());
-			lblS3PunkteGesamt.setText(currentSpieler.getPunktestand().getValue().toString() + " Punkte");
-
-		}
-
-	}
-
-
+        // Placement 3 (Bronze)
+        if (spielerliste.size() >= 3) {
+            Spieler s3 = spielerliste.get(2);
+            lblS3Name.setText(s3.getName());
+            lblS3PunkteGesamt.setText(s3.getPunktestand().get() + " Punkte");
+            rectPlatz3.getStyleClass().add(s3.getName().toLowerCase().replace(" ", ""));
+            rectPlatz3.setVisible(true);
+        } else {
+            lblS3Name.setText("");
+            lblS3PunkteGesamt.setText("");
+            rectPlatz3.setVisible(false);
+        }
+    }
 }
