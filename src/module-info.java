e40caa4af7884cab8822fba78148eb5fc6b7f832
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-03-04 15:20:29 +0100

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,14 +1,18 @@
 module buzzer.app {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;     
+    requires javafx.graphics;
+    requires javafx.base;
+
     requires com.pi4j.core;
+    requires com.pi4j.plugin.raspberrypi;
+    requires com.pi4j.plugin.pigpio;
+    requires com.pi4j.library.pigpio;
+
     requires java.net.http;
     requires org.slf4j;
-    
-    exports application;
 
+    exports application;
     opens application to javafx.fxml;
     opens view to javafx.fxml;
-
 }
