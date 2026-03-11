c847b269c31b3e6e12f5a73ae8532b937358188c
KeepCalmStayCalm <keepcalmstaycalm@hotmail.com>
2026-03-11 13:48:23 +0100

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -3,22 +3,26 @@ module buzzer.app {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
-    
+
     // Pi4J modules
     requires com.pi4j;
     requires com.pi4j.plugin.raspberrypi;
     requires com.pi4j.plugin.pigpio;
-    
+
     // Logging
     requires org.slf4j;
-    
-    // JSON parsing - REQUIRED FOR API NICKNAME FETCHING
+
+    // JSON parsing (for nickname API)
     requires com.google.gson;
-    
-    // Open packages for JavaFX reflection
+
+    // === NEW LINES THAT FIX YOUR ERROR ===
+    requires java.net.http;
+    requires java.prefs;
+
+    // Open packages for JavaFX FXML
     opens view to javafx.fxml;
     opens application to javafx.fxml;
-    
+
     // Export packages
     exports application;
     exports view;
